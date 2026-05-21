25ce5e800667f8bcfec9e17d2b1c8c04bbc58c6a
Parth Thummar <parththummar@gmail.com>
2026-05-21 16:25:24 -0400

    fix: prefix release tag with v in download URL

--- Casks/quietfinance.rb
+++ Casks/quietfinance.rb
@@ -2,7 +2,7 @@ cask "quietfinance" do
   version "2.6.0"
   sha256 "64902685c4c79c64319677f9629cce0265f537b7584f05e147543fd896719896"  # set by scripts/release.sh output
 
-  url "https://github.com/quietapps/QuietFinance/releases/download/#{version}/QuietFinance-#{version}.zip",
+  url "https://github.com/quietapps/QuietFinance/releases/download/v#{version}/QuietFinance-#{version}.zip",
       verified: "github.com/quietapps/QuietFinance/"
   name "Quiet Finance"
   desc "Track your net worth. Offline. No subscriptions. No cloud."
